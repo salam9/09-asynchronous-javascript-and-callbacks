90675e3ac519dcefe84e144785e6da48ed135b3c
Abdulsalam Y Alahmri <salam9@outlook.com>
2019-04-08 01:09:47 +0300

    Update main.js

--- starter-code/iife-exercise/js/main.js
+++ starter-code/iife-exercise/js/main.js
@@ -2,3 +2,6 @@
 // The function will automatically execute and count up every second until the specified argument.
 // Use the setTimeout function to count up.
 // Hint: a second is the timer passed * 1000 (milliseconds).
+setTimeout(function timer(t){
+    console.log( "Hello world" );
+}, t*1000 );
